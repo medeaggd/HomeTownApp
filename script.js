1144cfa1a0c37dbeab6050fb5384417c461704bb
Megan Quinn <35813625+medeaggd@users.noreply.github.com>
2018-05-14 16:46:33 -0500

    updates to all
    Changed to js library in html instead of jq.
    Took out some code for hamburger and drawer in main css.
    Added styles for hamburger and drawer into specs css - playing with display instead of transitions.
    Changed js file from jq with suggestions from mentor Marcello.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //// TODO: Add an if statement that checks viewport size, if larger than 601px, it should remove the button? Add back if lower?
-let hamburger = $("#hamburger");
-
-hamburger.addEventListener('click', function(e) {
-  drawer.classList.toggle('open');
-  e.stopPropagation();
+document.getElementByID("hamburger").addEventListener('click', function(e) {
+  let drawer = document.getElementByID("drawer");
+  if (drawer.style.display === "none") {
+      drawer.style.display = "block";
+  } else {
+      drawer.style.display = "none";
+  }
+//  e.stopPropagation();
 });
